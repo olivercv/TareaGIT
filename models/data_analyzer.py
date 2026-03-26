657b106c84b794eebc81b33e87e112bbf4f0c3e2
Victor Achocalla <vachocalla@gmail.com>
2026-03-25 22:16:30 -0400

    Se agrega comentario en data_analizer y strategy_model

--- models/data_analyzer.py
+++ models/data_analyzer.py
@@ -4,10 +4,15 @@ from models.strategy_model import AnalysisStrategy
 
 class DataAnalyzer:
     def __init__(self, strategy: AnalysisStrategy):
+
+        # Inicializa el DataAnalyzer con una estrategia de análisis.
+
         self._strategy = strategy
     
     def set_strategy(self, strategy: AnalysisStrategy):
+        # Permite cambiar la estrategia de análisis en tiempo de ejecución.
         self._strategy = strategy
     
     def execute(self, data):
+        # Ejecuta el análisis utilizando la estrategia actual.
         return self._strategy.analyze(data)
--- models/strategy_model.py
+++ models/strategy_model.py
@@ -4,6 +4,9 @@ from abc import ABC, abstractmethod
 # Strategy (Interfaz)
 # ============================
 class AnalysisStrategy(ABC):
+
+    # Define la interfaz para las estrategias de análisis.
+    
     @abstractmethod
     def analyze(self, data):
         pass
